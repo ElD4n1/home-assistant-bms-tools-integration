7cc394a22fcb162bec250fb3b67a7d43258802b9
Daniel Hofer <daniel_hofer_1@hotmail.com>
2022-04-15 17:55:47 +0200

    Increase update interval to 3 secs

--- custom_components/bmstools/__init__.py
+++ custom_components/bmstools/__init__.py
@@ -77,7 +77,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         _LOGGER,
         name="BMS Tools",
         update_method=async_update_data,
-        update_interval=timedelta(seconds=1),
+        update_interval=timedelta(seconds=3),
     )
 
     await coordinator.async_config_entry_first_refresh()
