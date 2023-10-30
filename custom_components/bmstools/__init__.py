02872415dc893f2c5020ac63019a9e3c29fa7a52
m4rcSA <43429867+m4rcSA@users.noreply.github.com>
2023-10-30 21:50:42 +0100

    Fix for Home Assistant 2023.5.3 (#1)
    Called async_setup_platforms instead of awaiting async_forward_entry_setups; this will fail in version 2023.3.

--- custom_components/bmstools/__init__.py
+++ custom_components/bmstools/__init__.py
@@ -87,7 +87,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         HASS_DATA_CLIENT: client,
     }
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
